3262aabf553d6c3d55431d97b56cefd0204d3135
vistajin <528189@qq.com>
2020-06-25 16:30:56 +0800

    Create ACE.cmd

---
+++ gcp/exam/ACE.cmd
@@ -0,0 +1,35 @@
+Question 1
+Every employee of your company has a Google account. Your operational team needs to manage a large number of 
+instances on Compute Engine. Each member of this team needs only administrative access to the servers. Your security 
+team wants to ensure that the deployment of credentials is operationally efficient and must be able to determine who 
+accessed a given instance. What should you do?
+A. Generate a new SSH key pair. Give the private key to each member of your team. Configure the public key in the 
+metadata of each instance.
+B. Ask each member of the team to generate a new SSH key pair and to send you their public key. Use a configuration 
+management tool to deploy those keys on each instance.
+C. Ask each member of the team to generate a new SSH key pair and to add the public key to their Gaggle account. 
+Grant the "compute.osAdminLogin" role to the Google group corresponding to this team.
+D. Generate a new SSH key pair. Give the private key to each member of your team. Configure the public key as a 
+project-wide public SSH key in your Cloud Platform project and allpw project-wide public SSH keys on each instance. 
+Answer: ??
+Question 2
+You need to create
+should you use?
+A. 0.0.0.0/0
+B. 10.0.0.0/8
+C. 172.16.0.0/12
+D. 192.168.0.0/16 
+Answer: B
+a custom VPC with a single subnet. The subnet's range must be as large as possible. Which range
+Question 3
+You want to select and configure a cost-effective solution for relational data on G.QQ.gle Cloud Platform. You are 
+working with a small set of operational data in one geographic location. You need to support point-in-time recovery. 
+What should you do?
+Verify that the enable binary logging option is selected.
+Select the create failover replicas option.
+C. Select Cloud Spanner. Set up your instance with 2 nodes.
+D. Select Cloud Spanner. Set up your instance as multi-regional.
+Answer: A
+. Select Cloud SQL (MySQL).
+B. Select Cloud SQL (MySQL).
+rmal text file
